410e16e8f0802edd1cd9875dcd9abd5a64662f54
janczakkonrad <50752633+janczakkonrad@users.noreply.github.com>
2021-01-08 21:40:31 +0100

    Updates

---
+++ precache-manifest.1c1e37c34d4097757eebe6d35e6faa99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbbc6c0f303148ed7095",
+    "url": "/app-rr/static/css/main.9a04cc01.chunk.css"
+  },
+  {
+    "revision": "cbbc6c0f303148ed7095",
+    "url": "/app-rr/static/js/main.327fa65a.chunk.js"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "10b595924a9112108439",
+    "url": "/app-rr/static/js/2.5695411f.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "68f2088c8210a7039ebb948ff714ec03",
+    "url": "/app-rr/index.html"
+  }
+];
